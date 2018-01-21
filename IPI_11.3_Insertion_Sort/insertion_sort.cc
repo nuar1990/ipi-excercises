0ad5461501c4417c3f8b75b79f867bf260d0197b
nuar1990 <nuar@nuar.de>
2018-01-21 17:54:52 +0100

    21.01.2018

--- IPI_11.3_Insertion_Sort/insertion_sort.cc
+++ IPI_11.3_Insertion_Sort/insertion_sort.cc
@@ -7,10 +7,11 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <list>
 #include <algorithm>
 
 template<typename T>
-void insertion_sort(std::vector<T>& vec);
+void insertion_sort(T& data);
 
 int main(){
 	std::vector<double> one={12.,-5.,0.,-1.,20.,9.};
@@ -18,25 +19,27 @@ int main(){
 	std::vector<int> three={2,5,4,-7,12,1};
 	std::vector<bool> four={false,true,false};
 	std::vector<char> five={'h','a','l','l','o'};
+	std::list<double> myList={12.,-5., 8., 0., -1., 20., 9.};
 
-	std::vector<double>::iterator it=one.begin();
-	std::vector<double>::iterator it2=one.end();
 
 	insertion_sort(one);
 	insertion_sort(two);
 	insertion_sort(three);
 	insertion_sort(four);
 	insertion_sort(five);
+	insertion_sort(myList);
 }
 
 
-
-
+//This function can handle any Container that specified an Iterator
+//vectors, lists incl.
 template<typename T>
-void insertion_sort(std::vector<T>& vec){
-    for (auto it = vec.begin(); it != vec.end(); ++it)
-        std::rotate(std::upper_bound(vec.begin(), it, *it), it, std::next(it));
-    for(auto it=vec.begin();it!=vec.end();++it)
+void insertion_sort(T& data)
+{
+	for (auto it = data.begin(); it != data.end(); ++it)
+        std::rotate(std::upper_bound(data.begin(), it, *it), it, std::next(it));
+    for(auto it=data.begin();it!=data.end();++it)
     	std::cout<<*it<<" ";
 	std::cout<<"\n";
 }
+
